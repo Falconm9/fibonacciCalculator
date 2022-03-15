7c4480d5777e7db19f98aa3237b8994cb2a5243f
unknown <joselfalconml@hotmail.com>
2022-03-14 22:42:34 -0600

    reactClient: set ui project

---
+++ react-client/src/containers/fibonacciCalculator/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FibonacciCalculator() {
+  return (
+    <div className="fibonacciCalc">
+      <h3>Calculate Fibonacci!</h3>
+    </div>
+  );
+}
+
+export default FibonacciCalculator;
